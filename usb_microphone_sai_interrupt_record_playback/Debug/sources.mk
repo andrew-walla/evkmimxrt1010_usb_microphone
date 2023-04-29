1d893ff8a19721dff655306f4df4bfb3563f42c0
andrew-walla <86935859+andrew-walla@users.noreply.github.com>
2023-04-29 12:56:46 +1000

    Initial commit
    Started with the sai_interrupt_record_playback EVK example and merged usb_host_audio_speaker. Code compiles and is ready for development to commence.

---
+++ usb_microphone_sai_interrupt_record_playback/Debug/sources.mk
@@ -0,0 +1,46 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+ASM_SRCS := 
+C_SRCS := 
+OBJ_SRCS := 
+O_SRCS := 
+S_SRCS := 
+S_UPPER_SRCS := 
+C_DEPS := 
+EXECUTABLES := 
+OBJS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+board \
+codec \
+codec/port/wm8960 \
+component/i2c \
+component/lists \
+component/osa \
+component/uart \
+device \
+drivers \
+source \
+startup \
+usb/device/class/audio \
+usb/device/class/ccid \
+usb/device/class/cdc \
+usb/device/class/cdc_rndis \
+usb/device/class/dfu \
+usb/device/class/hid \
+usb/device/class/msc \
+usb/device/class/phdc \
+usb/device/class/printer \
+usb/device/class \
+usb/device/class/video \
+usb/device/source/ehci \
+usb/device/source \
+usb/host/class \
+usb/host \
+usb/phy \
+utilities \
+xip \
+
---
+++ usb_microphone_sai_interrupt_record_playback/source/app.c
@@ -0,0 +1,374 @@
+/*
+ * Copyright (c) 2016, Freescale Semiconductor, Inc.
+ * Copyright 2016-2020 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "usb_host_config.h"
+#include "usb_host.h"
+#include "fsl_device_registers.h"
+#include "usb_host_hid.h"
+#include "audio_speaker.h"
+#include "fsl_common.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "board.h"
+#include "fsl_debug_console.h"
+#include "fsl_sai.h"
+#include "fsl_codec_common.h"
+
+#include "fsl_wm8960.h"
+#include "fsl_codec_adapter.h"
+#if (defined(FSL_FEATURE_SOC_SYSMPU_COUNT) && (FSL_FEATURE_SOC_SYSMPU_COUNT > 0U))
+#include "fsl_sysmpu.h"
+#endif /* FSL_FEATURE_SOC_SYSMPU_COUNT */
+#include "app.h"
+#include "usb_phy.h"
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+/* SAI instance and clock */
+#define DEMO_CODEC_WM8960
+#define DEMO_SAI              SAI1
+#define DEMO_SAI_CHANNEL      (0)
+#define DEMO_SAI_IRQ          SAI1_IRQn
+#define DEMO_SAITxIRQHandler  SAI1_IRQHandler
+#define DEMO_SAI_TX_SYNC_MODE kSAI_ModeAsync
+#define DEMO_SAI_RX_SYNC_MODE kSAI_ModeSync
+#define DEMO_SAI_MCLK_OUTPUT  true
+#define DEMO_SAI_MASTER_SLAVE kSAI_Master
+
+#define DEMO_AUDIO_DATA_CHANNEL (2U)
+#define DEMO_AUDIO_BIT_WIDTH    kSAI_WordWidth16bits
+#define DEMO_AUDIO_SAMPLE_RATE  (kSAI_SampleRate16KHz)
+#define DEMO_AUDIO_MASTER_CLOCK DEMO_SAI_CLK_FREQ
+
+/* Select Audio/Video PLL (786.48 MHz) as sai1 clock source */
+#define DEMO_SAI1_CLOCK_SOURCE_SELECT (2U)
+/* Clock pre divider for sai1 clock source */
+#define DEMO_SAI1_CLOCK_SOURCE_PRE_DIVIDER (3U)
+/* Clock divider for sai1 clock source */
+#define DEMO_SAI1_CLOCK_SOURCE_DIVIDER (15U)
+/* Get frequency of sai1 clock */
+#define DEMO_SAI_CLK_FREQ                                                        \
+    (CLOCK_GetFreq(kCLOCK_AudioPllClk) / (DEMO_SAI1_CLOCK_SOURCE_DIVIDER + 1U) / \
+     (DEMO_SAI1_CLOCK_SOURCE_PRE_DIVIDER + 1U))
+
+/* I2C instance and clock */
+#define DEMO_I2C LPI2C1
+
+/* Select USB1 PLL (480 MHz) as master lpi2c clock source */
+#define DEMO_LPI2C_CLOCK_SOURCE_SELECT (0U)
+/* Clock divider for master lpi2c clock source */
+#define DEMO_LPI2C_CLOCK_SOURCE_DIVIDER (5U)
+/* Get frequency of lpi2c clock */
+#define DEMO_I2C_CLK_FREQ ((CLOCK_GetFreq(kCLOCK_Usb1PllClk) / 8) / (DEMO_LPI2C_CLOCK_SOURCE_DIVIDER + 1U))
+
+#define BOARD_MASTER_CLOCK_CONFIG()
+#define BUFFER_SIZE   (1024U)
+#define BUFFER_NUMBER (4U)
+#ifndef DEMO_CODEC_VOLUME
+#define DEMO_CODEC_VOLUME 100U
+#endif
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+usb_host_handle g_hostHandle;
+wm8960_config_t wm8960Config = {
+    .i2cConfig = {.codecI2CInstance = BOARD_CODEC_I2C_INSTANCE, .codecI2CSourceClock = BOARD_CODEC_I2C_CLOCK_FREQ},
+    .route     = kWM8960_RoutePlaybackandRecord,
+    .leftInputSource  = kWM8960_InputDifferentialMicInput3,
+    .rightInputSource = kWM8960_InputDifferentialMicInput2,
+    .playSource       = kWM8960_PlaySourceDAC,
+    .slaveAddress     = WM8960_I2C_ADDR,
+    .bus              = kWM8960_BusI2S,
+    .format = {.mclk_HZ = 6144000U, .sampleRate = kWM8960_AudioSampleRate16KHz, .bitWidth = kWM8960_AudioBitWidth16bit},
+    .master_slave = false,
+};
+codec_config_t boardCodecConfig = {.codecDevType = kCODEC_WM8960, .codecDevConfig = &wm8960Config};
+
+/*
+ * AUDIO PLL setting: Frequency = Fref * (DIV_SELECT + NUM / DENOM)
+ *                              = 24 * (32 + 77/100)
+ *                              = 786.48 MHz
+ */
+const clock_audio_pll_config_t audioPllConfig = {
+    .loopDivider = 32,  /* PLL loop divider. Valid range for DIV_SELECT divider value: 27~54. */
+    .postDivider = 1,   /* Divider after the PLL, should only be 1, 2, 4, 8, 16. */
+    .numerator   = 77,  /* 30 bit numerator of fractional loop divider. */
+    .denominator = 100, /* 30 bit denominator of fractional loop divider */
+};
+AT_NONCACHEABLE_SECTION_ALIGN(static uint8_t Buffer[BUFFER_NUMBER * BUFFER_SIZE], 4);
+sai_handle_t txHandle = {0}, rxHandle = {0};
+static uint32_t tx_index = 0U, rx_index = 0U;
+volatile uint32_t emptyBlock = BUFFER_NUMBER;
+extern codec_config_t boardCodecConfig;
+codec_handle_t codecHandle;
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+extern void USB_HostClockInit(void);
+extern void USB_HostIsrEnable(void);
+extern void USB_HostTaskFn(void *param);
+void BOARD_InitHardware(void);
+extern void USB_AudioTask(void *arg);
+extern void USB_KeypadTask(void *arg);
+extern usb_status_t USB_HostAudioEvent(usb_device_handle deviceHandle,
+                                       usb_host_configuration_handle configurationHandle,
+                                       uint32_t eventCode);
+extern usb_status_t USB_HostKeypadEvent(usb_device_handle deviceHandle,
+                                        usb_host_configuration_handle configurationHandle,
+                                        uint32_t eventCode);
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+void USB_OTG1_IRQHandler(void)
+{
+    USB_HostEhciIsrFunction(g_hostHandle);
+}
+
+void USB_HostClockInit(void)
+{
+    usb_phy_config_struct_t phyConfig = {
+        BOARD_USB_PHY_D_CAL,
+        BOARD_USB_PHY_TXCAL45DP,
+        BOARD_USB_PHY_TXCAL45DM,
+    };
+
+    CLOCK_EnableUsbhs0PhyPllClock(kCLOCK_Usbphy480M, 480000000U);
+    CLOCK_EnableUsbhs0Clock(kCLOCK_Usb480M, 480000000U);
+    USB_EhciPhyInit(CONTROLLER_ID, BOARD_XTAL0_CLK_HZ, &phyConfig);
+}
+
+void USB_HostIsrEnable(void)
+{
+    uint8_t irqNumber;
+
+    uint8_t usbHOSTEhciIrq[] = USBHS_IRQS;
+    irqNumber                = usbHOSTEhciIrq[CONTROLLER_ID - kUSB_ControllerEhci0];
+/* USB_HOST_CONFIG_EHCI */
+
+/* Install isr, set priority, and enable IRQ. */
+#if defined(__GIC_PRIO_BITS)
+    GIC_SetPriority((IRQn_Type)irqNumber, USB_HOST_INTERRUPT_PRIORITY);
+#else
+    NVIC_SetPriority((IRQn_Type)irqNumber, USB_HOST_INTERRUPT_PRIORITY);
+#endif
+    EnableIRQ((IRQn_Type)irqNumber);
+}
+
+void USB_HostTaskFn(void *param)
+{
+    USB_HostEhciTaskFunction(param);
+}
+/*!
+ * @brief host callback function.
+ *
+ * device attach/detach callback function.
+ *
+ * @param deviceHandle           device handle.
+ * @param configurationHandle attached device's configuration descriptor information.
+ * @param eventCode           callback event code, please reference to enumeration host_event_t.
+ *
+ * @retval kStatus_USB_Success              The host is initialized successfully.
+ * @retval kStatus_USB_NotSupported         The application don't support the configuration.
+ */
+usb_status_t USB_HostEvent(usb_device_handle deviceHandle,
+                           usb_host_configuration_handle configurationHandle,
+                           uint32_t eventCode)
+{
+    usb_status_t status = kStatus_USB_Success;
+    switch (eventCode & 0x0000FFFFU)
+    {
+        case kUSB_HostEventAttach:
+            USB_HostKeypadEvent(deviceHandle, configurationHandle, eventCode);
+            status = USB_HostAudioEvent(deviceHandle, configurationHandle, eventCode);
+            break;
+
+        case kUSB_HostEventNotSupported:
+            usb_echo("device not supported.\r\n");
+            break;
+
+        case kUSB_HostEventEnumerationDone:
+            USB_HostKeypadEvent(deviceHandle, configurationHandle, eventCode);
+            status = USB_HostAudioEvent(deviceHandle, configurationHandle, eventCode);
+            break;
+
+        case kUSB_HostEventDetach:
+            USB_HostKeypadEvent(deviceHandle, configurationHandle, eventCode);
+            status = USB_HostAudioEvent(deviceHandle, configurationHandle, eventCode);
+            break;
+
+        case kUSB_HostEventEnumerationFail:
+            usb_echo("enumeration failed\r\n");
+            break;
+
+        default:
+            break;
+    }
+    return status;
+}
+
+/*!
+ * @brief app initialization.
+ */
+void APP_init(void)
+{
+    usb_status_t status = kStatus_USB_Success;
+
+    USB_HostClockInit();
+
+#if ((defined FSL_FEATURE_SOC_SYSMPU_COUNT) && (FSL_FEATURE_SOC_SYSMPU_COUNT))
+    SYSMPU_Enable(SYSMPU, 0);
+#endif /* FSL_FEATURE_SOC_SYSMPU_COUNT */
+
+    status = USB_HostInit(CONTROLLER_ID, &g_hostHandle, USB_HostEvent);
+    if (status != kStatus_USB_Success)
+    {
+        usb_echo("host init error\r\n");
+        return;
+    }
+    USB_HostIsrEnable();
+
+    usb_echo("host init done\r\n");
+}
+
+
+void BOARD_EnableSaiMclkOutput(bool enable)
+{
+    if (enable)
+    {
+        IOMUXC_GPR->GPR1 |= IOMUXC_GPR_GPR1_SAI1_MCLK_DIR_MASK;
+    }
+    else
+    {
+        IOMUXC_GPR->GPR1 &= (~IOMUXC_GPR_GPR1_SAI1_MCLK_DIR_MASK);
+    }
+}
+
+static void rx_callback(I2S_Type *base, sai_handle_t *handle, status_t status, void *userData)
+{
+    if (kStatus_SAI_RxError == status)
+    {
+        /* Handle the error. */
+    }
+    else
+    {
+        emptyBlock--;
+    }
+}
+
+static void tx_callback(I2S_Type *base, sai_handle_t *handle, status_t status, void *userData)
+{
+    if (kStatus_SAI_TxError == status)
+    {
+        /* Handle the error. */
+    }
+    else
+    {
+        emptyBlock++;
+    }
+}
+
+/*!
+ * @brief Main function
+ */
+int main(void)
+{
+    sai_transfer_t xfer;
+    sai_transceiver_t saiConfig;
+
+    BOARD_ConfigMPU();
+    BOARD_InitBootPins();
+    BOARD_InitBootClocks();
+    CLOCK_InitAudioPll(&audioPllConfig);
+    BOARD_InitDebugConsole();
+    APP_init();
+
+    /*Clock setting for LPI2C*/
+    CLOCK_SetMux(kCLOCK_Lpi2cMux, DEMO_LPI2C_CLOCK_SOURCE_SELECT);
+    CLOCK_SetDiv(kCLOCK_Lpi2cDiv, DEMO_LPI2C_CLOCK_SOURCE_DIVIDER);
+
+    /*Clock setting for SAI1*/
+    CLOCK_SetMux(kCLOCK_Sai1Mux, DEMO_SAI1_CLOCK_SOURCE_SELECT);
+    CLOCK_SetDiv(kCLOCK_Sai1PreDiv, DEMO_SAI1_CLOCK_SOURCE_PRE_DIVIDER);
+    CLOCK_SetDiv(kCLOCK_Sai1Div, DEMO_SAI1_CLOCK_SOURCE_DIVIDER);
+
+    /*Enable MCLK clock*/
+    BOARD_EnableSaiMclkOutput(true);
+
+    PRINTF("SAI interrupt record playback example started!\n\r");
+
+    /* SAI init */
+    SAI_Init(DEMO_SAI);
+    SAI_TransferTxCreateHandle(DEMO_SAI, &txHandle, tx_callback, NULL);
+    SAI_TransferRxCreateHandle(DEMO_SAI, &rxHandle, rx_callback, NULL);
+
+    /* I2S mode configurations */
+    SAI_GetClassicI2SConfig(&saiConfig, DEMO_AUDIO_BIT_WIDTH, kSAI_Stereo, 1U << DEMO_SAI_CHANNEL);
+    saiConfig.syncMode    = DEMO_SAI_TX_SYNC_MODE;
+    saiConfig.masterSlave = DEMO_SAI_MASTER_SLAVE;
+    SAI_TransferTxSetConfig(DEMO_SAI, &txHandle, &saiConfig);
+    saiConfig.syncMode = DEMO_SAI_RX_SYNC_MODE;
+    SAI_TransferRxSetConfig(DEMO_SAI, &rxHandle, &saiConfig);
+
+    /* set bit clock divider */
+    SAI_TxSetBitClockRate(DEMO_SAI, DEMO_AUDIO_MASTER_CLOCK, DEMO_AUDIO_SAMPLE_RATE, DEMO_AUDIO_BIT_WIDTH,
+                          DEMO_AUDIO_DATA_CHANNEL);
+    SAI_RxSetBitClockRate(DEMO_SAI, DEMO_AUDIO_MASTER_CLOCK, DEMO_AUDIO_SAMPLE_RATE, DEMO_AUDIO_BIT_WIDTH,
+                          DEMO_AUDIO_DATA_CHANNEL);
+
+    /* master clock configurations */
+    BOARD_MASTER_CLOCK_CONFIG();
+
+    /* Use default setting to init codec */
+    if (CODEC_Init(&codecHandle, &boardCodecConfig) != kStatus_Success)
+    {
+        assert(false);
+    }
+    if (CODEC_SetVolume(&codecHandle, kCODEC_PlayChannelHeadphoneLeft | kCODEC_PlayChannelHeadphoneRight,
+                        DEMO_CODEC_VOLUME) != kStatus_Success)
+    {
+        assert(false);
+    }
+    while (1)
+    {
+#if ((defined USB_HOST_CONFIG_EHCI) && (USB_HOST_CONFIG_EHCI))
+        USB_HostEhciTaskFunction(g_hostHandle);
+#endif
+        USB_HostTaskFn(g_hostHandle);
+        USB_KeypadTask(NULL);
+        USB_AudioTask(NULL);
+        if (emptyBlock > 0)
+        {
+            xfer.data     = Buffer + rx_index * BUFFER_SIZE;
+            xfer.dataSize = BUFFER_SIZE;
+            if (kStatus_Success == SAI_TransferReceiveNonBlocking(DEMO_SAI, &rxHandle, &xfer))
+            {
+                rx_index++;
+            }
+            if (rx_index == BUFFER_NUMBER)
+            {
+                rx_index = 0U;
+            }
+        }
+        if (emptyBlock < BUFFER_NUMBER)
+        {
+            xfer.data     = Buffer + tx_index * BUFFER_SIZE;
+            xfer.dataSize = BUFFER_SIZE;
+            if (kStatus_Success == SAI_TransferSendNonBlocking(DEMO_SAI, &txHandle, &xfer))
+            {
+                tx_index++;
+            }
+            if (tx_index == BUFFER_NUMBER)
+            {
+                tx_index = 0U;
+            }
+        }
+    }
+}
